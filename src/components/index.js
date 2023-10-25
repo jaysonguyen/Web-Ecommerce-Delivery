67174450d8f92e13ce64659baabbc3bc91fcfad6
ThaoMy3172 <my.nguyen@smartlook.com.vn>
2023-10-25 13:51:53 +0700

    fix input file

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,7 @@
 import Sidebar from "./layout/Sidebar";
 import Dropdown from "./Dropdown/DropDown";
 import AdminHeader from "./layout/AminHeader";
-import SearchText from "./template/input/SearchText";
+import SearchText from "./template/Input/SearchText";
 import Notification from "./template/Modal/Notification";
-import Input from "./template/input/Input";
+import Input from "./template/Input/Input";
 export { Sidebar, Dropdown, AdminHeader, SearchText, Notification, Input };
