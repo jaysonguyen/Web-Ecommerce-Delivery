7311c6a9de83428ce124216544b81c44592f9c23
Jayson Nguyen <buleberry.nguyen@gmail.com>
2023-10-31 13:37:02 +0700

    fix: redirect axios

--- src/pages/admin/staff/StaffPage.jsx
+++ src/pages/admin/staff/StaffPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { MyTable } from "../../../components/template/table/MyTable/MyTable";
-import { Plus } from "phosphor-react";
+import { Plus, CaretLeft } from "phosphor-react";
 import { AddStaff } from "../../../components";
 import { ICON_SIZE_BIG } from "../../../utils/constraint";
 import { getStaffList, getShipperList } from "../../../services/StaffService";
@@ -10,6 +10,7 @@ import "../../../assets/css/Pages/staff.css";
 
 export const StaffPage = () => {
   const [staffs, setStaffs] = useState([]);
+  const [isShowAdd, setIsShowAdd] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -17,8 +18,8 @@ export const StaffPage = () => {
     try {
       const data = await getStaffList();
       if (data) {
-        setStaffs(data);
-        dispatch(consumerSlice.actions.setStaffList([data]));
+        setStaffs(data.data);
+        dispatch(consumerSlice.actions.setStaffList([data.data]));
       }
     } catch (error) {
       console.log(error);
@@ -29,8 +30,8 @@ export const StaffPage = () => {
     try {
       const data = await getShipperList();
       if (data) {
-        setStaffs(data);
-        dispatch(consumerSlice.actions.setShipperList([data]));
+        setStaffs(data.data);
+        dispatch(consumerSlice.actions.setShipperList([data.data]));
       }
     } catch (error) {
       console.log(error);
@@ -47,29 +48,41 @@ export const StaffPage = () => {
 
   return (
     <div className="padding-body">
-      <div className="pd-bt-10 staff_page_header flex-center-between">
-        <h3>Staff management</h3>
-        <button className="btnAdd">
-          {" "}
-          <Plus size={ICON_SIZE_BIG} />
-          Add
-        </button>
-      </div>
-      <div className="staff_page_content">
-        <div className="tab_container">
-          <button onClick={getStaff} className="tab_item btnAdd">
-            Staff
-          </button>
-          <button onClick={getShipper} className="tab_item btnAdd">
-            Shipper
-          </button>
+      {!isShowAdd && (
+        <>
+          <div className="pd-bt-10 staff_page_header flex-center-between">
+            <h3>Staff management</h3>
+            <button onClick={() => setIsShowAdd(true)} className="btnAdd">
+              {" "}
+              <Plus size={ICON_SIZE_BIG} />
+              Add
+            </button>
+          </div>
+          <div className="staff_page_content">
+            <div className="tab_container">
+              <button onClick={getStaff} className="tab_item btnAdd">
+                Staff
+              </button>
+              <button onClick={getShipper} className="tab_item btnAdd">
+                Shipper
+              </button>
+            </div>
+            <MyTable list={staffs && staffs} showCheckBox={true} />
+          </div>
+        </>
+      )}
+      {isShowAdd && (
+        <div className="add_employee_container">
+          <div className="go_back_button_container">
+            <CaretLeft
+              size={ICON_SIZE_BIG}
+              onClick={() => setIsShowAdd(false)}
+            />
+          </div>
+          <h3>Add staff</h3>
+          <AddStaff />
         </div>
-        <MyTable list={staffs && staffs} showCheckBox={true} />
-      </div>
-      <div className="add_employee_container">
-        <h3>Add staff</h3>
-        <AddStaff />
-      </div>
+      )}
     </div>
   );
 };
