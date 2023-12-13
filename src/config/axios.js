a042c5d43f173bfaa1dd39f1b19e0e60c0dd7fd9
GreeN7LeaF <thaomy310702@gmail.com>
2023-12-13 09:58:41 +0700

    host 1

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: "https://e6ff-183-80-39-61.ngrok.io",
 });
 
 instance.defaults.withCredentials = true;
